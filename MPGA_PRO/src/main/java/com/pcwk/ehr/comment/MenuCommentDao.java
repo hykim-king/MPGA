cd1b2861d43ab96b501d60e48eee3517fd2afc12
wns14 <wns14@192.168.219.100>
2021-09-06 00:10:32 +0900

    오류수정

--- MPGA_PRO/src/main/java/com/pcwk/ehr/comment/MenuCommentDao.java
+++ MPGA_PRO/src/main/java/com/pcwk/ehr/comment/MenuCommentDao.java
@@ -5,15 +5,15 @@ import java.sql.SQLException;
 public interface MenuCommentDao {
 
 	//등록!
-	int doInsert(CommentLikeVO comment) throws SQLException;
+	int doInsert(MenuCommentVO comment) throws SQLException;
 
 	//셀렉트원!
-	CommentLikeVO SelectOne(CommentLikeVO inVO);
+	MenuCommentVO SelectOne(MenuCommentVO inVO);
 
 	//수정! (다시 수정해야함)
-	int doUpdate(CommentLikeVO comment) throws SQLException;
+	int doUpdate(MenuCommentVO comment) throws SQLException;
 
 	//삭제! 
-	int doDelete(CommentLikeVO comment) throws SQLException;
+	int doDelete(MenuCommentVO comment) throws SQLException;
 
 }
--- MPGA_PRO/src/main/java/com/pcwk/ehr/comment/MenuCommentDaoImpl.java
+++ MPGA_PRO/src/main/java/com/pcwk/ehr/comment/MenuCommentDaoImpl.java
@@ -7,80 +7,70 @@ import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
-
-import com.pcwk.ehr.member.domain.UserVO;
+import com.pcwk.ehr.comment.MenuCommentVO;
 
 public class MenuCommentDaoImpl implements MenuCommentDao {
 
 	final Logger LOG = LoggerFactory.getLogger(getClass());
 	private DataSource dataSource;
 	private JdbcTemplate jdbcTemplate;
-	
-	
-	//등록!
-	@Override
-	public int doInsert(final CommentLikeVO comment)throws SQLException {
-	int flag = 0;
-	
-	StringBuilder sb=new StringBuilder(100);
-	sb.append(" INSERT INTO menu_comment(seq,menu_num,member_num,contents,reg_dt) \n");
-	sb.append(" VALUES (?,?,?,?,sysdate)						  				  \n");
-	LOG.debug("=======================================");
-	LOG.debug("sql=\n" + sb.toString());
-	LOG.debug("param=" + comment.toString());
-	LOG.debug("=======================================");
-	
-	Object[] args = { comment.getSeq(),
-				   	  comment.getMenuNum(),
-					  comment.getMemberNum(),
-					  comment.getContents(),
-					  comment.getRegDt()
-					};
-	LOG.debug("args=" + args);
-	this.jdbcTemplate.update(sb.toString(), args);
-	LOG.debug("flag=" + flag);
-	return flag;
-	
+
+	// 등록!
+
+	public int doInsert(final MenuCommentVO comment) throws SQLException {
+		int flag = 0;
+
+		StringBuilder sb = new StringBuilder(100);
+		sb.append(" INSERT INTO menu_comment(seq,menu_num,member_num,contents,reg_dt) \n");
+		sb.append(" VALUES (?,?,?,?,sysdate)						  				  \n");
+		LOG.debug("=======================================");
+		LOG.debug("sql=\n" + sb.toString());
+		LOG.debug("param=" + comment.toString());
+		LOG.debug("=======================================");
+
+		Object[] args = { comment.getSeq(), comment.getMenuNum(), comment.getMemberNum(), comment.getContents(),
+				comment.getRegDt() };
+		LOG.debug("args=" + args);
+		this.jdbcTemplate.update(sb.toString(), args);
+		LOG.debug("flag=" + flag);
+		return flag;
+
 	}
-	
-	//셀렉트원!
+
+	// 셀렉트원!
 	@Override
-	public CommentLikeVO SelectOne (CommentLikeVO inVO) {
-		CommentLikeVO outVO = null;
-		
+	public MenuCommentVO SelectOne(MenuCommentVO inVO) {
+		MenuCommentVO outVO = null;
+
 		StringBuilder sb = new StringBuilder();
 		sb.append("  ");
 		return outVO;
-		
-		
+
 	}
-	
-	//수정! (다시 수정해야함)
+
+	// 수정! (다시 수정해야함)
 	@Override
-	public int doUpdate (CommentLikeVO comment) throws SQLException {
-        int flag = 0;
-        
-        StringBuilder sb = new StringBuilder();
-        sb.append(" UPDATE menu_comment          \n");
-        sb.append("      					     \n");
-        sb.append("        					     \n");
-        sb.append("         					 \n");
-        LOG.debug("=========================================");
+	public int doUpdate(MenuCommentVO comment) throws SQLException {
+		int flag = 0;
+
+		StringBuilder sb = new StringBuilder();
+		sb.append(" UPDATE menu_comment          \n");
+		sb.append("      					     \n");
+		sb.append("        					     \n");
+		sb.append("         					 \n");
+		LOG.debug("=========================================");
 		LOG.debug("sql=\n" + sb.toString());
 		LOG.debug("param=" + comment.toString());
-		LOG.debug("=========================================");	
-		Object[] args = { comment.getContents(),
-						  comment.getMemberNum(),
-						  comment.getMenuNum(),
-						  comment.getRegDt(),
-						  comment.getSeq()
-		};
-		
+		LOG.debug("=========================================");
+		Object[] args = { comment.getContents(), comment.getMemberNum(), comment.getMenuNum(), comment.getRegDt(),
+				comment.getSeq() };
+		return flag;
+
 	}
-	
-	//삭제! 
+
+	// 삭제!
 	@Override
-	public int doDelete(CommentLikeVO comment) throws SQLException {
+	public int doDelete(MenuCommentVO comment) throws SQLException {
 		int flag = 0;
 		StringBuilder sb = new StringBuilder();
 		sb.append(" DELETE FROM menu_comment \n");
@@ -89,13 +79,12 @@ public class MenuCommentDaoImpl implements MenuCommentDao {
 		LOG.debug("sql=\n" + sb.toString());
 		LOG.debug("param=" + comment.toString());
 		LOG.debug("=========================================");
-		Object[] args = { comment.getSeq()};
+		Object[] args = { comment.getSeq() };
 		LOG.debug("args=" + args);
-		flag = this.jdbcTemplate.update(sb.toString(), args);		
+		flag = this.jdbcTemplate.update(sb.toString(), args);
 		LOG.debug("flag=" + flag);
 		return flag;
-			
+
 	}
-	
+
 }
-	

439d6a012b79f566fbd6f25a8580e1f9d879b214
김정훈 <89445661+JeongHoonKimm@users.noreply.github.com>
2021-09-12 22:18:02 +0900

    Add files via upload

--- MPGA_PRO/src/main/java/com/pcwk/ehr/notice/dao/NoticeDAOImpl.java
+++ MPGA_PRO/src/main/java/com/pcwk/ehr/notice/dao/NoticeDAOImpl.java
@@ -8,7 +8,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.pcwk.ehr.notice.vo.NoticeVO;
+import com.pcwk.ehr.notice.domain.NoticeVO;
 @Repository
 public class NoticeDAOImpl implements NoticeDAO {
 
@@ -16,33 +16,42 @@ public class NoticeDAOImpl implements NoticeDAO {
 	private SqlSession session;
 	static String namespace = "com.pcwk.ehr.notice";
 	@Override
-	public void noticeInsert(NoticeVO noticeVO) throws Exception {
-		session.insert(namespace+".noticeInsert", noticeVO);
+	public void doInsert(NoticeVO noticeVO) throws Exception {
+		session.insert(namespace+".doInsert", noticeVO);
 
 	}
 
 	@Override
-	public NoticeVO noticeRead(Integer seq) throws Exception {
+	public NoticeVO doRead(Integer seq) throws Exception {
 		
-		return session.selectOne(namespace+".noticeRead", seq);
+		return session.selectOne(namespace+".doRead", seq);
 	}
 
 	@Override
-	public void noticeUpdate(NoticeVO noticeVO)  throws Exception {
-		session.update(namespace+".noticeUpdate", noticeVO);
-
-	}
-
-	@Override
-	public void noticeDelete(Integer seq) throws Exception {
-		session.delete(namespace+".noticeDelete", seq);
+	public void doUpdate(NoticeVO noticeVO)  throws Exception {
+		session.update(namespace+".doUpdate", noticeVO);
 
 	}
 
 	@Override
-	public List<NoticeVO> noticeListAll() throws Exception {
-		return session.selectList(namespace+".noticeListAll");
+	public void doDelete(Integer seq) throws Exception {
+		session.delete(namespace+".doDelete", seq);
+
+	}
+
+	@Override
+	public List<NoticeVO> getAll() throws Exception {
+		return session.selectList(namespace+".getAll");
 		
+	}
+	@Override
+	public List<NoticeVO> doSelect01() throws Exception {
+		return session.selectList(namespace+".doSelect01");
+	}
+	
+	@Override
+	public List<NoticeVO> doSelect02() throws Exception {
+		return session.selectList(namespace+".doSelect02");
 	}
 
 }

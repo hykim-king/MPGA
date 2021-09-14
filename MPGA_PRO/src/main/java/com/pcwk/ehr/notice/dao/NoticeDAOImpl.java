ea85e7b84def6415f4f68473cb9919569b2cb5f0
김정훈 <89445661+JeongHoonKimm@users.noreply.github.com>
2021-09-14 21:10:04 +0900

    Add files via upload

---
+++ MPGA_PRO/src/main/java/com/pcwk/ehr/notice/dao/NoticeDAOImpl.java
@@ -0,0 +1,67 @@
+package com.pcwk.ehr.notice.dao;
+
+import java.util.List;
+
+
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.pcwk.ehr.notice.domain.NoticeVO;
+import com.pcwk.ehr.notice.util.NoticePagingVO;
+@Repository
+public class NoticeDAOImpl implements NoticeDAO {
+  
+	@Autowired
+	private SqlSession session;
+	static String namespace = "com.pcwk.ehr.notice";
+	@Override
+	public void doInsert(NoticeVO noticeVO) throws Exception {
+		session.insert(namespace+".doInsert", noticeVO);
+
+	}
+
+	@Override
+	public NoticeVO doRead(Integer seq) throws Exception {
+		
+		return session.selectOne(namespace+".doRead", seq);
+	}
+
+	@Override
+	public void doUpdate(NoticeVO noticeVO)  throws Exception {
+		session.update(namespace+".doUpdate", noticeVO);
+
+	}
+
+	@Override
+	public void doDelete(Integer seq) throws Exception {
+		session.delete(namespace+".doDelete", seq);
+
+	}
+
+	@Override
+	public List<NoticeVO> getAll() throws Exception {
+		return session.selectList(namespace+".getAll");
+		
+	}
+	@Override
+	public List<NoticeVO> doSelect01() throws Exception {
+		return session.selectList(namespace+".doSelect01");
+	}
+	
+	@Override
+	public List<NoticeVO> doSelect02() throws Exception {
+		return session.selectList(namespace+".doSelect02");
+	}
+	
+	public int doCountFAQ() {
+	
+		return session.selectOne(namespace+".doCountFAQ");
+	}
+	
+	public List<NoticeVO> doSelectFAQ(NoticePagingVO vo) {
+		return session.selectList(namespace+".doSelectFAQ", vo);
+	}
+	
+}
---
+++ MPGA_PRO/src/main/java/com/pcwk/ehr/notice/service/NoticeService.java
@@ -0,0 +1,23 @@
+package com.pcwk.ehr.notice.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.pcwk.ehr.notice.domain.NoticeVO;
+import com.pcwk.ehr.notice.util.NoticePagingVO;
+
+
+public interface NoticeService {
+	public void doInsert(NoticeVO noticeVO) throws Exception ;// throws Exception;
+	  public NoticeVO doRead(Integer seq) throws Exception;// throws Exception;
+	  public void doUpdate(NoticeVO noticeVO) throws Exception;// throws Exception;
+	  public void doDelete(Integer seq) throws Exception;
+	  public List<NoticeVO> getAll() throws Exception;
+	  public List<NoticeVO> doSelect01() throws Exception;
+	  public List<NoticeVO> doSelect02() throws Exception;
+		public int doCountFAQ();
+		   
+		// 페이징 처리 게시글 조회
+		public List<NoticeVO> doSelectFAQ(NoticePagingVO vo);
+}
